3176c5699f3d013394d4123b9ab23dc46a0b51a1
Parker Stafford <parker.stafford92@gmail.com>
2021-02-09 10:34:42 -0800

    Recursive solution

---
+++ leetcode/yellow/generateParenthesis.js
@@ -0,0 +1,37 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var generateParenthesis = function(n) {
+  const result = [];
+  function generator(string, left, right) {
+
+    // add a left
+    if (left < n) {
+      generator(string + '(', left + 1, right)
+    }
+    // add a right
+    if (right < n && right < left) {
+      generator(string + ')', left, right + 1)
+    }
+    if (left === n && right === n) {
+      result.push(string);
+    }
+  }
+  generator('', 0, 0)
+  return result;
+};
