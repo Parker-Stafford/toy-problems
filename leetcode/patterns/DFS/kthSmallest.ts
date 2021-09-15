1236dd66dc1b161bbc3ba2e272879050098f325b
Parker Stafford <parker.stafford92@gmail.com>
2021-09-15 14:28:25 -0700

    implement dfs and bubble sort method

---
+++ leetcode/patterns/DFS/kthSmallest.ts
@@ -0,0 +1,73 @@
+/* Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+                                   i
+Output: 3
+
+[1, 2, 3]
+       
+
+  return last element of array
+Constraints:
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize? */
+
+// Definition for a binary tree node.
+ class TreeNode {
+     val: number
+     left: TreeNode | null
+     right: TreeNode | null
+     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+     }
+ }
+ 
+
+ function kthSmallest(root: TreeNode | null, k: number): number {
+  // make array of length k filled with infinity
+  let smallArr = new Array(k).fill(Infinity);
+  // perform dfs on tree
+  dfsWithBubble(root, smallArr);
+  // return last element of array
+  return smallArr[smallArr.length - 1];
+};
+
+
+function dfsWithBubble(node: TreeNode, arr: number[]): void {
+  if (!node) return;
+  // compare value at each node to last element in array
+  if (node.val < arr[arr.length - 1]) {
+    // less than we will replace last element in array with current nodes value
+    arr[arr.length - 1] = node.val;
+    // call bubble function on array
+    bubble(arr);
+  }
+  // dfs
+  dfsWithBubble(node.left, arr);
+  dfsWithBubble(node.right, arr);
+
+}
+
+function bubble(arr: number[]): void {
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] < arr[i - 1]) {
+      [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+    } else break;
+  }
+}
+
+
+
+
+
+
+
